be2fc7e2c55f448749feac501adc2a3d16fa7de1
Emilio G. Busnelli <emiliobusnelli11@gmail.com>
2019-09-11 21:53:03 -0500

    Week 3 - Hello World Version Control
    Commiting Hello World assignment to github. INITIAL COMMIT AND PUSH

---
+++ HelloWorld/HelloWorld/HelloWorldPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class HelloWorldPage : ContentPage
+    {
+        public HelloWorldPage()
+        {
+            InitializeComponent();
+        }
+
+        void chgDisplay(object sender, System.EventArgs e)
+        {
+            if(lblDisplay.Text == "Hey Joe, you seeing this?"){
+                lblDisplay.Text = "";
+            }
+            else if(lblDisplay.Text == ""){
+                lblDisplay.Text = "Haha, gotcha!";
+            }
+            else{
+                lblDisplay.Text = "Hey Joe, you seeing this?";
+            }
+
+        }
+    }
+}
