475a55dc5583d4cc860d3d66cbed8e300e8a9905
Emilio G. Busnelli <emiliobusnelli11@gmail.com>
2019-09-16 20:18:31 -0500

    Add Master Pro
    Created our first App!

---
+++ AddMaster/AddMaster/AddMasterPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.Forms;
+
+namespace AddMaster
+{
+    public partial class AddMasterPage : ContentPage
+    {
+        public AddMasterPage()
+        {
+            InitializeComponent();
+        }
+
+        void Add_Clicked(object sender, System.EventArgs e)
+        {
+            double dblFirstNum, dblSecondNum;
+
+            // try to parse txtFirstNum Text, if successful, output it to dblFirstNum
+            double.TryParse(txtFirstNum.Text,out dblFirstNum);
+            double.TryParse(txtSecondNum.Text,out dblSecondNum);
+
+            lblDisplay.Text = (dblFirstNum + dblSecondNum).ToString();
+        }
+
+        void Clear_Clicked(object sender, System.EventArgs e)
+        {
+            txtFirstNum.Text = string.Empty;
+            txtSecondNum.Text = string.Empty;
+            lblDisplay.Text = "0";
+        }
+    }
+}
