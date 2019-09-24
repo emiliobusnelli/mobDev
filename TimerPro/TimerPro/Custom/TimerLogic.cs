b32077e56cc55a244bef600f591f5edfe0b1d39f
Emilio G. Busnelli <emiliobusnelli11@gmail.com>
2019-09-23 20:18:27 -0500

    Week 5 - Timer Pro
    Timer Pro assignment

---
+++ TimerPro/TimerPro/Custom/TimerLogic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace TimerPro.Custom
+{
+    public class TimerLogic
+    {
+        // _example the underscore means that it cannot be used outside of this class
+        private int _intSec;
+        private int _intMin;
+        private int _intHour;
+
+        public TimerLogic()
+        {
+            Reset();
+        }
+
+        public void Reset()
+        {
+            _intSec = 0;
+            _intMin = 0;
+            _intHour = 0;
+        }
+
+        public void SetTickCount()
+        {
+            _intSec++;
+            if(_intSec == 60){
+                _intMin++;
+                _intSec = 0;
+            }
+            if(_intMin == 60){
+                _intHour++;
+                _intMin = 0;
+            }
+        }
+
+        public string GetFormattedTimeString(){
+            return _intHour.ToString().PadLeft(2, '0') + ":" + _intMin.ToString().PadLeft(2, '0') + ":" + _intSec.ToString().PadLeft(2, '0');
+        }
+    }
+}
---
+++ TimerPro/TimerPro/TimerProPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Xamarin.Forms;
+using TimerPro.Custom;
+using System;
+
+namespace TimerPro
+{
+    public partial class TimerProPage : ContentPage
+    {
+        // o = object in otimerLogic
+        TimerLogic otimerLogic;
+        bool isRunning;
+
+        public TimerProPage()
+        {
+            InitializeComponent();
+            // takes class and instantiates it into an object
+            otimerLogic = new TimerLogic();
+        }
+        
+        void Start_Clicked(object sender, System.EventArgs e)
+        {
+            btnStop.IsEnabled = true;
+            btnStart.IsEnabled = false;
+            isRunning = true;
+
+            // lamda expression is a function that needs to have true or false returned
+            Device.StartTimer(TimeSpan.FromSeconds(1),() => {
+                if (isRunning)
+                {
+                    otimerLogic.SetTickCount();
+                    lblDisplay.Text = otimerLogic.GetFormattedTimeString();
+                }
+                return isRunning;
+            });
+        }
+
+        void Stop_Clicked(object sender, System.EventArgs e)
+        {
+            btnStart.IsEnabled = true;
+            btnStop.IsEnabled = false;
+            isRunning = false;
+
+        }
+
+        void Reset_Clicked(object sender, System.EventArgs e)
+        {
+            otimerLogic.Reset();
+            lblDisplay.Text = otimerLogic.GetFormattedTimeString();
+            btnStart.IsEnabled = true;
+            btnStop.IsEnabled = false;
+            isRunning = false;
+        }
+
+        // the moment the view is visible to the end user
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            btnStart.IsEnabled = true;
+            btnStop.IsEnabled = false;
+            lblDisplay.Text = "00:00:00";
+        }
+    }
+}
