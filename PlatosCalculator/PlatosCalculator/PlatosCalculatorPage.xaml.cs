67cfce77bb36eed486e09f525821a15e0b65f0be
Emilio G. Busnelli <emiliobusnelli11@gmail.com>
2019-10-08 14:13:26 -0500

    Platos Calculator COMPLETED

---
+++ PlatosCalculator/PlatosCalculator/PlatosCalculatorPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Xamarin.Forms;
+
+namespace PlatosCalculator
+{
+    public partial class PlatosCalculatorPage : ContentPage
+    {
+        
+
+        public PlatosCalculatorPage()
+        {
+            InitializeComponent();
+
+            pkrGender.Items.Add("Male");
+            pkrGender.Items.Add("Female");
+        }
+
+        void Handle_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            var selGender = pkrGender.Items[pkrGender.SelectedIndex];
+            lblOptAge.Text = "???";
+            txtAge.Text = "";
+        }
+
+        void Calc_Clicked(object sender, System.EventArgs e)
+        {
+            var selGender = pkrGender.SelectedItem;
+            double dblAge;
+
+
+            double.TryParse(txtAge.Text, out dblAge); 
+
+
+            if(selGender.Equals("Male")){
+                lblOptAge.Text = ((dblAge / 2) + 7).ToString();
+            }
+            else{
+                lblOptAge.Text = ((dblAge * 2) - 14).ToString();
+            }
+        }
+    }
+}
