06a07764c8660c369be3b894da08c4d069076d44
Emilio G. Busnelli <emiliobusnelli11@gmail.com>
2019-10-07 21:45:11 -0500

    DateNite App and DistanceConverter App FINISHED

---
+++ DistanceConverter/DistanceConverter/Views/AboutPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DistanceConverter.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+
+            Title = "About Me";
+        }
+    }
+}
---
+++ DistanceConverter/DistanceConverter/views/DistanceConverterPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+using DistanceConverter.Views;
+
+namespace DistanceConverter
+{
+    public partial class DistanceConverterPage : ContentPage
+    {
+        public DistanceConverterPage()
+        {
+            InitializeComponent();
+
+            Title = "Distanceee Converter";
+
+            ToolbarItem tbi = new ToolbarItem();
+            tbi.Text = "About";
+
+            tbi.Clicked += delegate {
+                Navigation.PushAsync(new AboutPage());
+            };
+            this.ToolbarItems.Add(tbi);
+        }
+    }
+}
