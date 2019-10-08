5a0ea70c5cdb0908aa7eed0d8a5dadf14844c200
Emilio G. Busnelli <emiliobusnelli11@gmail.com>
2019-10-08 11:23:57 -0500

    Carousel Demo

---
+++ CaouselDemo/CaouselDemo/CaouselDemoPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace CaouselDemo
+{
+    public partial class CaouselDemoPage : CarouselPage
+    {
+        public CaouselDemoPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            // this demos the next page
+            base.OnAppearing();
+            Device.StartTimer(TimeSpan.FromMilliseconds(200), () => {
+                this.CurrentPage = this.Children[1];
+                return false;
+            });
+            // This brings you back to main page
+            Device.StartTimer(TimeSpan.FromMilliseconds(1000), () => {
+                this.CurrentPage = this.Children[0];
+                return false;
+            });
+        }
+
+
+    }
+}
