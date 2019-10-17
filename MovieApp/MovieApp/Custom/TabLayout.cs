e71b8d733c0a9f15df7ed008dc6a9e0785410164
Emilio Busnelli <emiliobusnelli11@gmail.com>
2019-10-17 14:52:41 -0500

    SaveStuff and MovieApp

---
+++ MovieApp/MovieApp/Custom/TabLayout.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+using MovieApp.Views;
+
+namespace MovieApp.Custom
+{
+    public class TabLayout : TabbedPage
+    {
+        public TabLayout()
+        {
+            Children.Add(new AddPage() { Title = "Add Movie", IconImageSource="add"});
+            Children.Add(new FindPage() { Title = "Find Movie", IconImageSource="find"});
+        }
+    }
+}
---
+++ MovieApp/MovieApp/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace MovieApp.Models
+{
+    public class Movie
+    {
+        // Properties
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Rating { get; set; }
+    }
+}
