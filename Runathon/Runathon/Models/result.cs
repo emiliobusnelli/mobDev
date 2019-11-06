eddb4db43e895ad14db11718f509afbcc35ba376
Emilio Busnelli <emiliobusnelli11@gmail.com>
2019-11-05 19:23:16 -0600

    Works on Mac Laptop!

--- Runathon/Runathon/Models/result.cs
+++ Runathon/Runathon/Models/result.cs
@@ -20,7 +20,7 @@ namespace Runathon.Models
         {  
             get 
             {
-                return placement + " Place    Ttime: " + time;
+                return placement + " Place    Time: " + time;
             }
         }
     }
